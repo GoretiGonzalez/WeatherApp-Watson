94db0da342df9308cd049e8062cd3de2fbc77255
Ruth Goreti Gonzalez Rodriguez <ruthgg@mx1.ibm.com>
2021-04-20 19:15:04 -0500

    Adding files

---
+++ settings.js
@@ -0,0 +1,5 @@
+exports.watson_text_speach_username = "817b8b3a-0e57-4650-b28f-9401f4ab3bfd";
+exports.watson_text_speach_password = "1zjgVBQAmfUo";
+exports.watson_text_speach_url = "https://stream.watsonplatform.net/text-to-speech/api/";
+
+exports.weather_key = "199c0a6f1b66f772acf7ed686b8c602a";
